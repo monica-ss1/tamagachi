1e2f18218210a4ddf1e278b17645cffb9dc43c77
Michelle <michellepineda255@gmail.com>
2025-12-09 19:44:49 -0800

    Fixed Menu! hopefully...

--- tamagachi/MenuView.swift
+++ tamagachi/MenuView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import SwiftData
 
 struct MenuView: View {
+    @State private var navigationPath = NavigationPath()
     var body: some View {
         NavigationStack {
             ZStack {
@@ -14,40 +15,49 @@ struct MenuView: View {
                     .scaledToFill()
                     .frame(width: 6, height: 60)
                     .offset(y: -260)
-                VStack(spacing: -80){
-                    NavigationLink(destination: GameView()) {
-                        Image("new game button")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 250, height: 250)
-                    }
-                    
-                    Button {
-                        loadTapped()
-                    } label: {
-                        Image("play button")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 250, height: 250)
-
-                    }
-                    Button {
-                        exitTapped()
-                    } label: {
-                        Image("exit button")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 250, height: 250)
+                NavigationStack(path:$navigationPath){
+                    VStack(spacing: -80){
+                        NavigationLink(value: "game") {
+                            Image("play button")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 250, height: 250)
+                        }
+                        Button {
+                            newTapped()
+                            navigationPath.append("game")
+                            
+                        } label: {
+                            Image("new game button")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 250, height: 250)
+                            
+                        }
+                        Button {
+                            exitTapped()
+                        } label: {
+                            Image("exit button")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 250, height: 250)
+                        }
+                        .navigationDestination(for: String.self) {
+                            value in
+                            if value == "game"{
+                                GameView()
+                            }
+                            
+                        }
                     }
                 }
             }
         }
     }
-
-    func loadTapped() {
-        print("Load tapped")
-        let LoadFunction = PetModel()
-        LoadFunction.loadStats()
+    func newTapped() {
+        print("New Game tapped")
+        let NewFunction = PetModel()
+        NewFunction.restart()
     }
 
     func exitTapped() {
